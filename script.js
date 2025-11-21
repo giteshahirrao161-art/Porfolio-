82494fcde8d420cce6863ce748c7bbf91bf464c1
Gitesh Ahirrao <giteshahirrao161@gmail.com>
2025-11-21 17:11:36 -0500

    Fix formatting of initProjectVideos function call

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     initResumeDownload();
     initContactForm();
     initSmoothScrolling();
-    initProjectVideos();
+    initProjectVideos();  
 });
 
 // Navigation functionality
