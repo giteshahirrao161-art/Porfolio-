785693bb1784bb32ae9f405597f42cf898aba526
Gitesh Ahirrao <giteshahirrao161@gmail.com>
2026-04-20 15:23:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
     initContactForm();
     initSmoothScrolling();
     initProjectVideos();  
-});
+});  
 
 // Navigation functionality
 function initNavigation() {
